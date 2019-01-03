d440ee4c5775572e47c8e7c402fa80af6c8ccce1
Anthony Clifton <aclifton@pillartechnology.com>
2019-01-03 11:48:35 -0600

    Deduped test steps

--- features/step_definitions/fizzBuzz.step.js
+++ features/step_definitions/fizzBuzz.step.js
@@ -6,67 +6,51 @@ import App from "../../src/App";
 const feature = loadFeature('./features/fizzBuzz.feature');
 
 defineFeature(feature, test => {
-    test('Returns a number when given a number not divisible by 3 or 5', ({given, when, then}) => {
-        const wrapper = mount(<App/>);
+    let wrapper;
 
-        given('Input contains 17', () => {
-            wrapper.find('input').simulate('change', {target: {value: '17'}});
+    beforeEach(() => {
+        wrapper = mount(<App/>);
+    });
+
+    const givenInputContainsX = given => {
+        given(/Input contains (\w+)/, input => {
+            wrapper.find('input').simulate('change', {target: {value: input}});
         });
+    };
 
+    const whenButtonIsClicked = when => {
         when('I click submit', () => {
             wrapper.find('button').simulate('click');
         });
+    };
 
-        then('17 is displayed', () => {
-            expect(wrapper.find('p').text()).toEqual('17');
+    const thenXIsDisplayed = then => {
+        then(/(\w+) is displayed/, result => {
+            expect(wrapper.find('p').text()).toEqual(result);
         });
+    };
+
+    test('Returns a number when given a number not divisible by 3 or 5', ({given, when, then}) => {
+        givenInputContainsX(given);
+        whenButtonIsClicked(when);
+        thenXIsDisplayed(then);
     });
 
     test('Returns fizz when given a number divisible by three', ({given, when, then}) => {
-        const wrapper = mount(<App/>);
-
-        given('Input contains 6', () => {
-            wrapper.find('input').simulate('change', {target: {value: '6'}});
-        });
-
-        when('I click submit', () => {
-            wrapper.find('button').simulate('click');
-        });
-
-        then('fizz is displayed', () => {
-            expect(wrapper.find('p').text()).toEqual('fizz');
-        });
+        givenInputContainsX(given);
+        whenButtonIsClicked(when);
+        thenXIsDisplayed(then);
     });
 
     test('Returns buzz when given a number divisible by five', ({given, when, then}) => {
-        const wrapper = mount(<App/>);
-
-        given('Input contains 10', () => {
-            wrapper.find('input').simulate('change', {target: {value: '10'}});
-        });
-
-        when('I click submit', () => {
-            wrapper.find('button').simulate('click');
-        });
-
-        then('buzz is displayed', () => {
-            expect(wrapper.find('p').text()).toEqual('buzz');
-        });
+        givenInputContainsX(given);
+        whenButtonIsClicked(when);
+        thenXIsDisplayed(then);
     });
 
     test('Returns fizzbuzz when given a number divisible by three and five', ({given, when, then}) => {
-        const wrapper = mount(<App/>);
-
-        given('Input contains 30', () => {
-            wrapper.find('input').simulate('change', {target: {value: '30'}});
-        });
-
-        when('I click submit', () => {
-            wrapper.find('button').simulate('click');
-        });
-
-        then('fizzbuzz is displayed', () => {
-            expect(wrapper.find('p').text()).toEqual('fizzbuzz');
-        });
+        givenInputContainsX(given);
+        whenButtonIsClicked(when);
+        thenXIsDisplayed(then);
     });
 });
